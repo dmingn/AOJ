0bebd2ba723e41434196c1605e18ee95198ab976
dmingn <dmingn@users.noreply.github.com>
2017-07-27 16:17:27 +0900

    ALDS1_14_A

--- ALDS1/ALDS1_14_A.cpp
+++ ALDS1/ALDS1_14_A.cpp
@@ -10,19 +10,19 @@ int main() {
 
     cin >> T >> P;
 
-    vector<pair<string, uint32_t>> sa;
+    vector<uint32_t> sa;
     for (uint32_t i = 0; i < T.length(); i++) {
-        sa.push_back(make_pair(T.substr(i, T.length()-i), i));
+        sa.push_back(i);
     }
 
-    sort(sa.begin(), sa.end());
+    sort(sa.begin(), sa.end(), [T](uint32_t a, uint32_t b) {return T.substr(a, T.length()-a) < T.substr(b, T.length()-b);});
 
-    auto itr_lb = lower_bound(sa.begin(), sa.end(), P, [](pair<string, uint32_t> p, string s) {return p.first < s;});
-    auto itr_ub = upper_bound(sa.begin(), sa.end(), P, [](string s, pair<string, uint32_t> p) {return s < p.first.substr(0, s.length());});
+    auto itr_lb = lower_bound(sa.begin(), sa.end(), P, [T](uint32_t i, string s) {return T.substr(i, T.length()-i) < s;});
+    auto itr_ub = upper_bound(itr_lb, sa.end(), P, [T](string s, uint32_t i) {return s < T.substr(i, T.length()-i).substr(0, s.length());});
 
     vector<uint32_t> indexes;
     for (auto itr = itr_lb; itr != itr_ub; itr++) {
-        indexes.push_back((*itr).second);
+        indexes.push_back(*itr);
     }
 
     sort(indexes.begin(), indexes.end());
