f009bd63e6f8077f26dedc9eab9250041cbdc1d1
dmingn <dmingn@users.noreply.github.com>
2017-07-27 22:19:38 +0900

    ALDS1_14_B

---
+++ ALDS1/ALDS1_14_B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string T, P;
+
+    cin >> T >> P;
+
+    uint64_t ht = 0, hp = 0, b = 1;
+    for (uint32_t i = 0; i < P.length(); i++) {
+        ht = (ht << 7) + T[i];
+        hp = (hp << 7) + P[i];
+        b = b << 7;
+    }
+
+    for (int32_t i = 0; i <= (int32_t)(T.length() - P.length()); i++) {
+        if (ht == hp) if (T.substr(i, P.length()) == P) printf("%d\n", i);
+        ht = (ht << 7) + T[i+P.length()] - b * T[i];
+    }
+
+    return 0;
+}
